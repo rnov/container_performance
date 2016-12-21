16f925306d6fee86b77ae9ef551e2274cfd6a9a6
as <as@DESKTOP-BA92AHU>
2016-12-21 12:02:20 +0100

    async_doTests function, search functions added, some improvments in already implemented functions

--- cont_test.cpp
+++ cont_test.cpp
@@ -7,69 +7,118 @@
 #include "container_test.h"
 
 #include<future>
-
+#include<type_traits>
+#include<iostream>
 //#include <boost/intrusive/list.hpp>
+
 
 // /*
 int main(){
 
     // create vector with random numbers and shuffle it
-    int ini_rand = 1, fin_rand = 50000; // 200000  -> 200k
-    int sorted_ini=1, sorted_fin = 50000; // 500000 -> 500k
-    int n_op = 20;  // nº operations
+    int ini_rand = 1, fin_rand = 20000; // 200000  -> 200k, 500000 -> 500k
+    int sorted_ini=1, sorted_fin = 20000; // 500000 -> 500k
+    unsigned int n_op = 10;  // nº operations
 
     // random numbers vector
-    auto rand_vector = generate_random_vector(ini_rand, fin_rand, 5000);
+    auto rand_vector = generate_random_vector(ini_rand, fin_rand, 2000);
 
     // generate dst container for the values in rand_vector
     std::vector<int> dst_container_vector;
     generate_seq_container(dst_container_vector, sorted_ini, sorted_fin);
 
-//     Operations
+	bin_search(dst_container_vector, rand_vector);
+	
 
-    auto rand_vector_2 = shuffle_container(std::begin(dst_container_vector), std::end(dst_container_vector)); // dst_container_vector
+//     Operations
+// /*
+	// multiprocessing
+	//auto fut = std::async(car<std::vector<int>, std::vector<int>>, dst_container_vector, rand_vector);
+	//auto fut2 = std::async(testc::doTests<std::vector<int>, std::vector<int>>, n_op, dst_container_vector, rand_vector, (testc::fParam<std::vector<int>, std::vector<int>>)insertSortetedContaniner);
+    //auto rand_vector_2 = shuffle_container(std::begin(dst_container_vector), std::end(dst_container_vector)); // dst_container_vector
 
     // VECTOR
 //    std::vector<testc::graph> some = testc::doTests(n_op, dst_container_vector, rand_vector_2,(testc::fParam<std::vector<int>, std::vector<int>>)deletefromContaniner);
-    std::vector<testc::graph> some = testc::doTests(n_op, dst_container_vector, rand_vector,(testc::fParam<std::vector<int>, std::vector<int>>)insertSortetedContaniner);
-    print_measures(some);
-    cvs_columns("insertVectorData.csv", some);
+//    std::vector<testc::graph> some = testc::doTests(n_op, dst_container_vector, rand_vector,(testc::fParam<std::vector<int>, std::vector<int>>)insertSortetedContaniner);
+//	std::vector<testc::graph> some = testc::doTests(n_op, dst_container_vector, rand_vector, (testc::fParam<std::vector<int>, std::vector<int>>)bin_search);
+//	std::vector<testc::graph> some = testc::doTests(n_op, dst_container_vector, rand_vector, (testc::fParam<std::vector<int>, std::vector<int>>)search);
+//	print_measures(some);
+//	cvs_columns("SearchVectorData.csv", some);
 
     // LIST
     std::list<int> dst_container_list;
     generate_seq_container(dst_container_list, sorted_ini, sorted_fin);
 //    std::vector<testc::graph> some_2 = testc::doTests(num_op, dst_container_list, rand_vector_2, (testc::fParam<std::list<int>, std::vector<int>>)deletefromContaniner);
-    std::vector<testc::graph> some_2 = testc::doTests(n_op, dst_container_list, rand_vector, (testc::fParam<std::list<int>, std::vector<int>>)insertSortetedContaniner);
-    testc::print_measures(some_2);
-    cvs_columns("insertListData.csv", some_2);
+//    std::vector<testc::graph> some_2 = testc::doTests(n_op, dst_container_list, rand_vector, (testc::fParam<std::list<int>, std::vector<int>>)insertSortetedContaniner);
+//	std::vector<testc::graph> some_2 = testc::doTests(n_op, dst_container_list, rand_vector, (testc::fParam<std::list<int>, std::vector<int>>)bin_search);
+//	std::vector<testc::graph> some_2 = testc::doTests(n_op, dst_container_list, rand_vector, (testc::fParam<std::list<int>, std::vector<int>>)search);
+//	testc::print_measures(some_2);
+//    cvs_columns("SearchListData.csv", some_2);
 
 //    // DQUEUE
     std::deque<int> dst_container_dqueue;
     generate_seq_container(dst_container_dqueue, sorted_ini, sorted_fin);
 //    std::vector<testc::graph> some_3 = testc::doTests(n_op, dst_container_dqueue, rand_vector_2, (testc::fParam<std::deque<int>, std::vector<int>>)deletefromContaniner);
-    std::vector<testc::graph> some_3 = testc::doTests(n_op, dst_container_dqueue, rand_vector, (testc::fParam<std::deque<int>, std::vector<int>>)insertSortetedContaniner);
-    testc::print_measures(some_3);
-    cvs_columns("insertDequeData.csv", some_3);
+//    std::vector<testc::graph> some_3 = testc::doTests(n_op, dst_container_dqueue, rand_vector, (testc::fParam<std::deque<int>, std::vector<int>>)insertSortetedContaniner);
+//	std::vector<testc::graph> some_3 = testc::doTests(n_op, dst_container_dqueue, rand_vector, (testc::fParam<std::deque<int>, std::vector<int>>)bin_search);
+//	std::vector<testc::graph> some_3 = testc::doTests(n_op, dst_container_dqueue, rand_vector, (testc::fParam<std::deque<int>, std::vector<int>>)search);
+//	testc::print_measures(some_3);
+//    cvs_columns("SearchDequeData.csv", some_3);
 
     // SET
     std::set<int> dst_container_set;
     generate_container(dst_container_set, sorted_ini, sorted_fin);
 //    std::vector<testc::graph> some_4 = testc::doTests(n_op, dst_container_set, rand_vector_2, (testc::fParam<std::set<int>, std::vector<int>>)deletefromContaniner);
-    std::vector<testc::graph> some_4 = testc::doTests(n_op, dst_container_set, rand_vector, (testc::fParam<std::set<int>, std::vector<int>>)insertSortetedContaniner);
-    testc::print_measures(some_4);
-    cvs_columns("insertSetData.csv", some_4);
+//    std::vector<testc::graph> some_4 = testc::doTests(n_op, dst_container_set, rand_vector, (testc::fParam<std::set<int>, std::vector<int>>)insertSortetedContaniner);
+//	std::vector<testc::graph> some_4 = testc::doTests(n_op, dst_container_set, rand_vector, (testc::fParam<std::set<int>, std::vector<int>>)bin_search);
+//	std::vector<testc::graph> some_4 = testc::doTests(n_op, dst_container_set, rand_vector, (testc::fParam<std::set<int>, std::vector<int>>)search);
+//	testc::print_measures(some_4);
+//    cvs_columns("SearchSetData.csv", some_4);
 //    print_Container(dst_container_set);
+
 
     //  Unordered Set
     std::unordered_set<int> dst_container_unset;
     generate_container(dst_container_unset, sorted_ini, sorted_fin);
 //    std::vector<testc::graph> some_5 = testc::doTests(n_op, dst_container_unset, rand_vector_2, (testc::fParam<std::unordered_set<int>, std::vector<int>>)deletefromContaniner);
-    std::vector<testc::graph> some_5 = testc::doTests(n_op, dst_container_unset, rand_vector, (testc::fParam<std::unordered_set<int>, std::vector<int>>)insertSortetedContaniner);
-//    testc::print_measures(some_5);
-    cvs_columns("insertUnsetData.csv", some_5);
+//    std::vector<testc::graph> some_5 = testc::doTests(n_op, dst_container_unset, rand_vector, (testc::fParam<std::unordered_set<int>, std::vector<int>>)insertSortetedContaniner);
+//	std::vector<testc::graph> some_5 = testc::doTests(n_op, dst_container_unset, rand_vector, (testc::fParam<std::unordered_set<int>, std::vector<int>>)bin_search);
+	//std::vector<testc::graph> some_5 = testc::doTests(n_op, dst_container_unset, rand_vector, ez_life(dst_container_unset, rand_vector)search);  // std::unordered_set<int>, std::vector<int>
+	//testc::print_measures(some_5);
+	std::cout << "start now!" << '\n';
+	std::vector<testc::graph> some_5async = testc::doTests_async(n_op, dst_container_unset, rand_vector, ez_life(dst_container_unset, rand_vector)search_sets);  // search_sets
+    testc::print_measures(some_5async);
+//    cvs_columns("SearchUnsetData.csv", some_5);
 //    print_Container(dst_container_set);
+
+	
+
+	// parallel
+	/*
+	auto fut2 = std::async(
+		testc::doTests<std::vector<int>, std::vector<int>>, n_op, dst_container_vector, rand_vector, (testc::fParam<std::vector<int>, std::vector<int>>)search);
+	auto fut3 = std::async(
+		testc::doTests<std::list<int>, std::vector<int>>, n_op, dst_container_list, rand_vector, (testc::fParam<std::list<int>, std::vector<int>>)search);
+	auto fut4 = std::async(
+		testc::doTests<std::deque<int>, std::vector<int>>, n_op, dst_container_dqueue, rand_vector, (testc::fParam<std::deque<int>, std::vector<int>>)search);
+	auto fut5 = std::async(
+		testc::doTests<std::set<int>, std::vector<int>>, n_op, dst_container_set, rand_vector, (testc::fParam<std::set<int>, std::vector<int>>)search2);
+	auto fut6 = std::async(
+		testc::doTests<std::unordered_set<int>, std::vector<int>>, n_op, dst_container_unset, rand_vector, (testc::fParam<std::unordered_set<int>, std::vector<int>>)search2);
+
+	std::vector<testc::graph> someq = fut2.get();
+	std::vector<testc::graph> someq2 = fut3.get();
+	std::vector<testc::graph> someq3 = fut4.get();
+	std::vector<testc::graph> someq4 = fut5.get();
+	std::vector<testc::graph> someq5 = fut6.get();
+	
+	cvs_columns("pVectorData.csv", someq);
+	cvs_columns("plistData.csv", someq2);
+	cvs_columns("pdequeData.csv", someq3);
+	cvs_columns("psetData.csv", someq4);
+	cvs_columns("punsetData.csv", someq5);
+	*/
 
     return 0;
 }
 // */
-
--- container_test.h
+++ container_test.h
@@ -8,11 +8,13 @@
 #include <chrono>
 #include <fstream>
 #include <iterator>
+#include<future>
+#include <tuple>
 
 /*********Crono*************/
 std::chrono::system_clock::time_point start, end;
 std::chrono::duration<double> elapsed_seconds;
-std::time_t end_time;
+//std::time_t end_time;
 
 /**********random**************/
 std::random_device rd;  // only used once to initialise (seed) engine
@@ -20,14 +22,35 @@ std::random_device rd;  // only used once to initialise (seed) engine
 /********cvs file*************/
 std::ofstream outfile;
 
-#define Print_time(type_container,fun, ...)\
-     std::cout<<"Measuring "<<type_container<<":"<<'\n';\
+#define ez_life(dst_cont, rand_cont)\
+	(testc::fParam<decltype(dst_cont), decltype(rand_cont)>)
+
+#define Print_time(fun, ...)\
      start = std::chrono::system_clock::now();\
      fun(__VA_ARGS__);\
      end = std::chrono::system_clock::now();\
      elapsed_seconds = end-start;\
-     end_time = std::chrono::system_clock::to_time_t(end);\
-     std::cout<< "  -elapsed time: " << elapsed_seconds.count() << " s\n";
+	 std::cout<< "  -elapsed time: " << elapsed_seconds.count() << " s\n";
+
+// checks if the given container is the right function at compile time.
+
+// vector, deque, list, (forward_list not implemented)
+template <template<class T, class =std::allocator<T>> class Container, typename T>
+bool is_sequence(Container<T>& container) {
+	return true;
+}
+
+// set (multiset not implemented)
+template<template<class T, class = std::less<T>, class =std::allocator<T>> class Container, typename T>
+bool is_set(Container<T>& container) {
+	return true;
+}
+
+// unordered_set (unordered_multiset not implemented)
+template< template<class T, class =std::hash<T>, class =std::equal_to<T>, class =std::allocator<T>> class Container, typename T, typename k>
+bool is_unset(k cont) {  // Container<T>& container
+	return Container<int> == cont;
+}
 
 /*********templates (vector, list, dqueue, set, unordered_set)*************/
 
@@ -64,7 +87,6 @@ std::vector<int> generate_random_vector(int ini, int end, int num=0){
             random_vec.push_back(distr(g));
         }
     }
-
     return random_vec;
 }
 
@@ -78,12 +100,6 @@ void generate_seq_container(Container<T>& container, int ini, int end){
     }
 }
 
-//template<
-//
-//        class Key,
-//        class Compare = std::less<Key>,
-//        class Allocator = std::allocator<Key>
-//> class set;
 //template <template<class Key, class= std::less<Key>, class= std::allocator<Key>> class Container, typename Key>
 //void generate_seq_container(Container<Key>& container, int ini, int end){
 template <typename Container>
@@ -107,6 +123,7 @@ std::vector<int> shuffle_container(iterator begin, iterator end){
     return sh_cont;
 }
 
+/// Insert
 // inserts in container, Container can be vector, list, dqueue and (Associative containers and Unordered associative containers)
 template <typename Container, typename  Vartype>
 void insertSortetedValue(Container& vec, Vartype& value){
@@ -136,25 +153,25 @@ void insertSortetedContaniner_iter(Container& cont, const Srccont& src_cont){
     }
 }
 
-// retruns a copy of the suffled container.
-//template <typename Container, typename T>
-//std::vector<T> shuffle_container(const Container& cont){
-//
-//    std::vector<T> sh_cont;
-//
-//    for(const auto& value: cont) sh_cont.push_back(value);
-//    std::mt19937 g(rd());
-//    std::shuffle(std::begin(sh_cont), std::end(sh_cont), g);
-//    return sh_cont;
-//}
 
+/// Delete
 template <typename Container, typename  Srccont>
 void deletefromContaniner(Container& cont, const Srccont& src_cont){
     assert(!cont.empty());
     for(const auto& value : src_cont) {
         auto pos = std::find(std::begin(cont), std::end(cont), value);
-        cont.erase(pos);
+		cont.erase(pos);
     }
+}
+
+template <typename Container, typename  Srccont>
+void deletefromContaninersafe(Container& cont, const Srccont& src_cont) {
+	assert(!cont.empty());
+	for (const auto& value : src_cont) {
+		auto pos = std::find(std::begin(cont), std::end(cont), value);
+		if(pos != std::end(cont))
+			cont.erase(pos);
+	}
 }
 
 template <typename Container, typename  Srccont>
@@ -167,49 +184,99 @@ void deletefromContaniner_iter(Container& cont, const Srccont& src_cont){
     }
 }
 
+/// Search
+
+// accepts any type of stl container, the return value is not important 
+// in case the container does not support random access iterator (like list, set, unordered_set...) the seach is not binary but linear
+template <typename Container, typename  Srccont>
+void bin_search(const Container& cont, const Srccont& src_cont) { 
+	for (auto& value : src_cont) {
+		std::binary_search(std::cbegin(cont), std::cend(cont), value);
+	}
+}
+
+template <typename Container, typename  Srccont>
+void search( Container& cont, const Srccont& src_cont) {
+	for (auto& value : src_cont) {
+		std::find(std::cbegin(cont), std::cend(cont), value);
+	}
+}
+
+template <typename Container, typename  Srccont>
+void search_sets(Container& cont, const Srccont& src_cont) {
+
+	assert(is_unset(cont));  // is_set(cont) || is_unset(cont)
+	for (auto& value : src_cont) {
+		cont.find(value);
+	}
+}
+
 /***************test************************/
 namespace testc {
 
-    // Data structure time/operation measures
-    struct graph {
-        graph() = default;
-        graph(unsigned int n_operations, double time) : noperations(n_operations), time(time) { };
+	// Data structure time/operation measures
+	struct graph {
+		graph() = default;
+		graph(unsigned int n_operations, double time) : noperations(n_operations), time(time) { };
+		unsigned int noperations;  // nº operation (inserts/deletes...)
+		double time;  // time took each
+	};
 
-        unsigned int noperations;  // nº operation (inserts/deletes...)
-        double time;  // time took each
-    };
+	// template pointers to functions
+	template<typename Container, typename T>
+	using fParam = void(*) ( Container& cont, const T& src_cont);
 
-    // template pointer function
-    template<typename Container, typename T>
-    using fParam = void (*) (Container& cont, const T& src_cont);
+	void print_measures(const std::vector<graph>& times) {
+		for (graph k : times) {
+			std::cout << "nº inserted values: " << k.noperations << " time: " << k.time << " sec" << std::endl;
+		}
+	}
 
-    void print_measures(const std::vector<graph>& times){
-        for(graph k : times){
-            std::cout<<"nº inserted values: "<<k.noperations<<" time: "<<k.time<<" sec"<<std::endl;
-        }
-    }
+	// auxiliar funtion for measuring time, used for async do test calls
+	template<typename Container, typename T >
+	decltype(auto) operation_time(Container& cont, const T& src_cont, unsigned int n_operations ,fParam<Container&, T> foo) {
+		Print_time(foo, cont, src_cont);
+		return std::make_tuple(n_operations, elapsed_seconds.count());
+	}
 
-    //std::vector<graph> doTests(int ntest, const Container &cont, const std::vector<T>& src_cont, fParam<Container,T> foo) {
-    template<typename Container, typename T >
-    std::vector<graph> doTests(int ntest, const Container& cont, const T& src_cont, fParam<Container,T> foo) {
-        assert(ntest <= src_cont.size());
-        std::vector<graph> values;
+	template<typename Container, typename T >
+	std::vector<graph> doTests(int ntest, Container& cont, const T& src_cont, fParam<Container&,T> foo) {
+		assert(ntest <= src_cont.size());
+		std::vector<graph> values;
 
-        auto steps = src_cont.size() / ntest;
-        for (auto operations = steps; operations <= src_cont.size(); operations += steps) {
-            auto copyContainer = cont;  // copy the container
-            T src_aux(std::begin(src_cont), std::begin(src_cont) + operations);  // range of values
+		auto steps = src_cont.size() / ntest;
+		for (auto operations = steps; operations <= src_cont.size(); operations += steps) {
+			auto copyContainer = cont;  // copy the container
+			T src_aux(std::begin(src_cont), std::begin(src_cont) + operations);  // range of values
+			Print_time(foo, copyContainer, src_aux);
+			values.push_back(graph(operations, elapsed_seconds.count()));
+		}
+		return values;
+	}
 
-//                std::cout << "copyContainer before operation " << copyContainer.size() << '\n';
-            Print_time("container", foo, copyContainer, src_aux);
-//                std::cout << "copyContainer after operation " << copyContainer.size() << " value of i: " << operations <<
-//                " value of steps: " << steps << "\n\n";
-            values.push_back(graph(operations, elapsed_seconds.count()));
-        }
-        return values;
-    }
-}
+	template<typename Container, typename T >
+	std::vector<graph> doTests_async(int ntest, Container& cont, const T& src_cont, fParam<Container&, T> foo) {
+		assert(ntest <= src_cont.size());
+		std::vector<graph> values;
 
+		auto steps = src_cont.size() / ntest;
+		std::vector<std::future< std::tuple<unsigned int, double> >> fut_res; // return tuple<unsigned int, double>
+		
+		for (auto operations = steps; operations <= src_cont.size(); operations += steps) {
+			auto copyContainer = cont;  // copy the container
+			T src_aux(std::begin(src_cont), std::begin(src_cont) + operations);  // range of values
+			// using 'insert' instead of 'push_back' becuase std::future<> objects are not CopyConstructible
+			fut_res.insert(std::cend(fut_res), std::async(std::launch::async, operation_time<decltype(cont), decltype(src_cont)>, copyContainer, src_aux, operations, foo));
+		}
+
+		for (auto& fut : fut_res) {
+			auto tuple = fut.get();
+			values.push_back(graph(std::get<0>(tuple), std::get<1>(tuple))); // 0 -> operation, 1 -> time
+		}
+		return values;
+	}
+
+} // testc
 
 /*************files cvs*************************/
 void cvs_columns(std::string name, const std::vector<testc::graph> data){
