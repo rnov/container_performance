1325daa698dda10ec1e370c151b03e78c2ff170a
rnov <rsmarinov@gmail.com>
2017-01-21 22:04:32 +0100

    adding wrappers.h , last commit changes  apply to this file

--- wrappers.h
+++ wrappers.h
@@ -1,24 +1,8 @@
 #include <vector>
+#include <functional>
+#include <iterator>
 
-#define ez_game(param)\
-		(testc::varidic<decltype(param)>)
-
-#define ez_game2(param, para2)\
-		(testc::varidic<decltype(param), decltype(para2)>)
-
-#define ez_game3(param, para2, para3)\
-		(testc::varidic<decltype(param), decltype(para2), decltype(para3)>)
-
-#define ez_game4(param, para2, para3, param4)\
-		(testc::varidic<decltype(param), decltype(para2), decltype(para3), decltype(para4)>)
-
-#define ez_game3b(param, param2, param3)\
-		(testc::varidic<decltype(param), decltype(param2), const decltype(param3)&>)
-
-#define ez_game4b(param, param2, param3, param4)\
-		(testc::varidic<decltype(param), decltype(param2), const decltype(param3)&, decltype(param4)>)
-
-// find param wrappers
+// alg_wrappers
 
 #define Binary_Search(param1, param2, type)\
 	(variadic<bool, decltype(param1), decltype(param2),const type&>)
@@ -29,12 +13,34 @@
 #define Lower_Bound(param1, param2, type)\
 	(variadic<decltype(param1), decltype(param1), decltype(param2), const type&>)
 
+#define Find(param1, param2, type)\
+	(variadic<decltype(param1), decltype(param1), decltype(param2), const type&>)
 
-template <typename ... Ts>
-using var = decltype(auto)(*) (Ts ... V_args);// decltype(auto) */
+#define Count(param1, param2, type)\
+	(variadic<std::iterator_traits<decltype(param1)>::difference_type, decltype(param1), decltype(param2), const type&>)
 
-template <typename ... Ts>
-using varidic = void(*) (Ts ... V_args);  // void
+#define Equal_Range(param1, param2, type)\
+	(variadic<std::pair<decltype(param1), decltype(param1)>, decltype(param1), decltype(param2), const type&>)
+
+#define Fill(param1, param2, type)\
+	(variadic<void, decltype(param1), decltype(param2), const type&>)
+ 
+//-----
+#define Sort(param1, param2)\
+	(variadic<void, decltype(param1), decltype(param2)>)
+
+#define Is_Sorted(param1, param2)\
+	(variadic<bool, decltype(param1), decltype(param2)>)
+
+#define Is_Sorted_Until(param1, param2)\
+	(variadic<decltype(param1), decltype(param1), decltype(param2)>)
+
+#define Max_Element(param1, param2)\
+	(variadic<decltype(param1), decltype(param1), decltype(param2)>)
+
+#define Reverse(param1, param2)\
+	(variadic<void, decltype(param1), decltype(param2)>)
+
 
 struct graph {
 	graph() = default;
@@ -46,23 +52,6 @@ struct graph {
 template <typename K, typename ... Ts>
 using variadic = K(*) (Ts ... V_args);  // void
 
-template <typename K, typename T>
-void alg_wrapper(T some, variadic<K, T> funcptr) {
-	//funcptr(some);
-	Print_time(funcptr, some);
-}
-
-template <typename K, typename T, typename T2>
-void alg_wrapper(T some, T2 some2, variadic<K, T, T2> funcptr) {
-	//funcptr(some, some2);
-	Print_time(funcptr, some, some2);
-}
-
-template <typename K, typename T, typename T2, typename T3>
-void alg_wrapper(T some, T2 some2, T3 some3, variadic<K, T, T2, T3> funcptr) {
-	//funcptr(some, some2, some3);
-	Print_time(funcptr, some, some2, some3);
-}
 
 // fixed adapted for 'find' operations (find works for binary_search, lower_bound, upper_bound)
 template <typename ReturnVariadic, typename ParamType, typename ParamType2, typename ParamType3>
@@ -86,7 +75,27 @@ std::vector<graph> alg_wrapper(int n_op, std::vector<ParamType3>vec, ParamType s
 	return values;
 }
 
-template<typename T, typename K, typename Iterator>
-std::vector<graph> find(int n_operation, std::vector<T> source, Iterator first, Iterator last, variadic<K, Iterator, Iterator, const T&> funcptr) {
-	return alg_wrapper(n_operation, source, first, last, (variadic<K, decltype(first), decltype(last), const T&>)funcptr);
+// fix it
+template <typename container,typename ReturnVariadic, typename ParamType, typename ParamType2>
+std::vector<graph> alg_wrapper(int n_op, container cont,ParamType some, ParamType2 some2, variadic<ReturnVariadic, ParamType, ParamType2> funcptr) {
+	assert(n_op <= vec.size());
+	
+	std::vector<graph> values;
+	for (auto operations = steps; operations <= vec.size(); operations += steps) {
+		start = std::chrono::system_clock::now();
+		for (const auto& value : src_aux)
+			funcptr(some, some2, value);
+		end = std::chrono::system_clock::now();
+
+		elapsed_seconds = end - start;
+		std::cout << "  -elapsed time: " << elapsed_seconds.count() << "sec nº operations: " << operations << '\n';  // just for tests
+		values.push_back(graph(operations, elapsed_seconds.count()));
+	}
+	return values;
 }
+
+/*
+template<typename T, typename K, typename Iterator>
+std::vector<graph> find_2(int n_operation, std::vector<T> source, Iterator first, Iterator last, variadic<K, Iterator, Iterator, const T&> funcptr) {
+	return alg_wrapper(n_operation, source, first, last, (variadic<K, decltype(first), decltype(last), const T&>)funcptr);
+}*/
